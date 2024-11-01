059d4466b1d55be7027e602912d4d3ec9cfbe158
PhanTruong12 <122134064+PhanTruong12@users.noreply.github.com>
2024-11-02 02:11:25 +0700

    Update DashboardChart.tsx

--- components/admin-management/DashboardChart.tsx
+++ components/admin-management/DashboardChart.tsx
@@ -23,27 +23,27 @@ ChartJS.register(
 );
 
 const data = {
-  labels: ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00'],
+  labels: ['Bụi Chill', 'Go N Chill', 'Summer SUP', 'Huy SUP', 'Camping SUP Đà Nẵng', 'Bơ SUP', 'Thể thao biển Đà Nẵng', 'Tùng Lê SUP', 'King SUP', 'SUP Đà Nẵng', 'Hani Glamping'],
   datasets: [
     {
-      label: 'Sales',
-      data: [30, 40, 28, 50, 40, 60, 70],
+      label: 'Revenue Month 1',
+      data: [2000000, 2400000, 5600000, 5000000, 3000000, 0, 0, 0, 0, 0, 0],
       borderColor: 'rgba(54, 162, 235, 1)',
       backgroundColor: 'rgba(54, 162, 235, 0.2)',
       borderWidth: 2,
-      tension: 0.4, // Làm cho đường biểu đồ mềm mại hơn
+      tension: 0.4,
     },
     {
-      label: 'Revenue',
-      data: [40, 50, 38, 60, 50, 70, 80],
+      label: 'Revenue Month 2',
+      data: [4000000, 4000000, 7000000, 2400000, 3000000, 6000000, 2000000, 3600000, 0, 0, 0],
       borderColor: 'rgba(75, 192, 192, 1)',
       backgroundColor: 'rgba(75, 192, 192, 0.2)',
       borderWidth: 2,
       tension: 0.4,
     },
     {
-      label: 'Customers',
-      data: [20, 30, 18, 40, 30, 50, 60],
+      label: 'Revenue Month 3',
+      data: [4400000, 4400000, 5600000, 6000000, 3600000, 7400000, 4000000, 3000000, 2400000, 1600000, 1600000],
       borderColor: 'rgba(255, 206, 86, 1)',
       backgroundColor: 'rgba(255, 206, 86, 0.2)',
       borderWidth: 2,
